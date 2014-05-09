b911d58b57e7798413eaefd98ca89584c211f313
Ali <ali_areskog@abtassoc.com>
2014-05-09 18:59:13 -0400

    updates to add demographics link to popups

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var markers = [
-{ lat:'34.256535', lon:'-92.052418', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602TRFLR5100I" target=_blank>ARCELORMITTAL PINE BLUFF</a><br>5100 INDUSTRIAL DR S, PINE BLUFF ARKANSAS 71602 (JEFFERSON), PINE BLUFF, AR 71602<br>TRI ID = 71602TRFLR5100I<br>Latitude 34.256535&nbsp; Longitude -92.052418<br>EJ 1 Mile: <a href="javascript:drawRace(0,1)">Race</a>, &nbsp;<a href="javascript:drawInc(0,1)">Income</a>, &nbsp;<a href="javascript:demographics(0,1)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(0,3)">Race</a>, &nbsp;<a href="javascript:drawInc(0,3)">Income</a>, &nbsp;<a href="javascript:demographics(0,3)">Demographics</a></font>', label:'ARCELORMITTAL PINE BLUFF', ej1:'0.607,0.448,0.018,0.402,0.581,0.003,0.393,0.167,0.244,0.085', ej3:'0.583,0.263,0.009,0.422,0.560,0.005,0.317,0.186,0.273,0.124' },
-{ lat:'34.257073', lon:'-92.062488', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602FLRDD3300N" target=_blank>BERENFIELD CONTAINERS SW LTD</a><br>3300 N HUTCHINSON ST, WHITE HALL ARKANSAS 71602 (JEFFERSON), WHITE HALL, AR 71602<br>TRI ID = 71602FLRDD3300N<br>Latitude 34.257073&nbsp; Longitude -92.062488<br>EJ 1 Mile: <a href="javascript:drawRace(1,1)">Race</a>, &nbsp;<a href="javascript:drawInc(1,1)">Income</a>, &nbsp;<a href="javascript:demographics(1,1)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(1,3)">Race</a>, &nbsp;<a href="javascript:drawInc(1,3)">Income</a>, &nbsp;<a href="javascript:demographics(1,3)">Demographics</a></font>', label:'BERENFIELD CONTAINERS SW LTD', ej1:'0.376,0.303,0.012,0.632,0.350,0.004,0.297,0.170,0.290,0.108', ej3:'0.514,0.245,0.010,0.491,0.490,0.006,0.299,0.183,0.275,0.136' },
+{ lat:'34.256535', lon:'-92.052418', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602TRFLR5100I" target=_blank>ARCELORMITTAL PINE BLUFF</a><br>5100 INDUSTRIAL DR S, PINE BLUFF ARKANSAS 71602 (JEFFERSON), PINE BLUFF, AR 71602<br>TRI ID = 71602TRFLR5100I<br>Latitude <span class="lat">34.256535</span>&nbsp; Longitude <span class="lon">-92.052418</span></font>', label:'ARCELORMITTAL PINE BLUFF', ej1:'0.607,0.448,0.018,0.402,0.581,0.003,0.393,0.167,0.244,0.085', ej3:'0.583,0.263,0.009,0.422,0.560,0.005,0.317,0.186,0.273,0.124' },
+{ lat:'34.257073', lon:'-92.062488', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602FLRDD3300N" target=_blank>BERENFIELD CONTAINERS SW LTD</a><br>3300 N HUTCHINSON ST, WHITE HALL ARKANSAS 71602 (JEFFERSON), WHITE HALL, AR 71602<br>TRI ID = 71602FLRDD3300N<br>Latitude <span class="lat">34.257073</span>&nbsp; Longitude <span class="lon">-92.062488</span><br>EJ 1 Mile: <a href="javascript:drawRace(1,1)">Race</a>, &nbsp;<a href="javascript:drawInc(1,1)">Income</a>, &nbsp;<a class="buffer1Single" href="javascript:void(0)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(1,3)">Race</a>, &nbsp;<a href="javascript:drawInc(1,3)">Income</a>, &nbsp;<a href="javascript:demographics(1,3)">Demographics</a></font>', label:'BERENFIELD CONTAINERS SW LTD', ej1:'0.376,0.303,0.012,0.632,0.350,0.004,0.297,0.170,0.290,0.108', ej3:'0.514,0.245,0.010,0.491,0.490,0.006,0.299,0.183,0.275,0.136' },
 { lat:'34.2656', lon:'-92.0261', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602GYLRD500MC" target=_blank>DELTA NATURAL KRAFT</a><br>1701 JEFFERSON PKWY, PINE BLUFF ARKANSAS 71611 (JEFFERSON), PINE BLUFF, AR 71611<br>TRI ID = 71602GYLRD500MC<br>Latitude 34.2656&nbsp; Longitude -92.0261<br>EJ 1 Mile: <a href="javascript:drawRace(2,1)">Race</a>, &nbsp;<a href="javascript:drawInc(2,1)">Income</a>, &nbsp;<a href="javascript:demographics(2,1)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(2,3)">Race</a>, &nbsp;<a href="javascript:drawInc(2,3)">Income</a>, &nbsp;<a href="javascript:demographics(2,3)">Demographics</a></font>', label:'DELTA NATURAL KRAFT', ej1:'0.973,0.421,0.002,0.027,0.965,0.000,0.296,0.235,0.358,0.037', ej3:'0.706,0.336,0.010,0.298,0.685,0.003,0.373,0.198,0.273,0.081' },
 { lat:'34.221569', lon:'-91.907406', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71611NTRNTFAIRF" target=_blank>EVERGREEN PACKAGING</a><br>5201 FAIRFIELD RD, PINE BLUFF ARKANSAS 71601 (JEFFERSON), PINE BLUFF, AR 71601<br>TRI ID = 71611NTRNTFAIRF<br>Latitude 34.221569&nbsp; Longitude -91.907406<br>EJ 1 Mile: <a href="javascript:drawRace(3,1)">Race</a>, &nbsp;<a href="javascript:drawInc(3,1)">Income</a>, &nbsp;<a href="javascript:demographics(3,1)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(3,3)">Race</a>, &nbsp;<a href="javascript:drawInc(3,3)">Income</a>, &nbsp;<a href="javascript:demographics(3,3)">Demographics</a></font>', label:'EVERGREEN PACKAGING', ej1:'0.500,0.152,0.063,0.531,0.438,0.000,0.231,0.231,0.308,0.077', ej3:'0.529,0.247,0.033,0.482,0.485,0.002,0.268,0.216,0.301,0.098' },
 { lat:'33.138611', lon:'-91.966667', html:'<font face=Arial size=1><a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71635GRGPCPAPER" target=_blank>GEORGIA-PACIFIC CROSSETT PLYWOOD/STUDMI LL COMPLEX</a><br>101 PLYWOOD MILL RD, CROSSETT ARKANSAS 71635 (ASHLEY), CROSSETT, AR 71635<br>TRI ID = 71635GRGPCPAPER<br>Latitude 33.138611&nbsp; Longitude -91.966667<br>EJ 1 Mile: <a href="javascript:drawRace(4,1)">Race</a>, &nbsp;<a href="javascript:drawInc(4,1)">Income</a>, &nbsp;<a href="javascript:demographics(4,1)">Demographics</a><br>EJ 3 Mile: <a href="javascript:drawRace(4,3)">Race</a>, &nbsp;<a href="javascript:drawInc(4,3)">Income</a>, &nbsp;<a href="javascript:demographics(4,3)">Demographics</a></font>', label:'GEORGIA-PACIFIC CROSSETT PLYWOOD/STUDMI LL COMPLEX', ej1:'0.693,0.284,0.014,0.308,0.672,0.004,0.413,0.094,0.250,0.133', ej3:'0.383,0.176,0.017,0.623,0.359,0.004,0.255,0.129,0.329,0.167' }
@@ -27,8 +27,9 @@ require([ "esri/map",
           "dojo/dom-class",
           "dojo/dom-construct",
           "dojo/on",
+          "dojo/query",
           "dojo/domReady!"
-        ], function(Map, SpatialReference, Popup, PopupTemplate, Geometry, Point, Multipoint, Circle, Extent, SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Font, TextSymbol, Graphic, GraphicsLayer, Color, domClass, domConstruct, on) { 
+        ], function(Map, SpatialReference, Popup, PopupTemplate, Geometry, Point, Multipoint, Circle, Extent, SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Font, TextSymbol, Graphic, GraphicsLayer, Color, domClass, domConstruct, on, query) { 
 
 var popup = Popup({highlight: false},domConstruct.create("div"));
 
@@ -73,6 +74,7 @@ var popup = Popup({highlight: false},domConstruct.create("div"));
 
     var bufferSymb = new SimpleFillSymbol();
     var buffer1Layer = new GraphicsLayer();
+    var buffer1SingleLayer = new GraphicsLayer();
     var buffer3Layer = new GraphicsLayer();
 
   $('.showPopup').click(function(){
@@ -84,69 +86,112 @@ var popup = Popup({highlight: false},domConstruct.create("div"));
     map.infoWindow.setTitle("");
     map.infoWindow.setContent(html);
     map.infoWindow.show(p);
-  })
+    $('.actionList').removeClass('hidden');
+  });
 
-    $('.buffer1').click(function(){
-      hideEverything("buffer1");
-      if(buffer1Layer._div === null){
-        for ( i in latLonArray.points){
-          buffer = new Circle({
-            center: latLonArray.points[i],
-            geodesic: true,
-            radius: 1,
-            radiusUnit: "esriMiles"
-          });
-          var bufferCircle = new Graphic(buffer, bufferSymb);
-          buffer1Layer.add(bufferCircle);
-        }
-        $('#legendInfo').html('<p>1 Mile Radius</p>');
-        map.addLayer(buffer1Layer);
-        buffer1Layer.show();
+  domConstruct.create("p", { "id": "ejMile1", "innerHTML": "EJ Mile 1: "}, query(".actionList", window.map.infoWindow.domNode)[0]);
+    domConstruct.create("p", { "id": "ejMile3", "innerHTML": "EJ Mile 3: "}, query(".actionList", window.map.infoWindow.domNode)[0]);
+  var buffer1Link = domConstruct.create("a", { "class": "buffer1Single", "innerHTML": "Demographics", "href": "javascript:void(0);"}, query("#ejMile1", window.map.infoWindow.domNode)[0]);
+  query('.buffer1Single').on("click", buffer1Single);
+
+  $('.buffer1').click(function(){
+    buffer1();
+  });
+
+  $('.buffer3').click(function(){
+    buffer3();
+  });
+
+  function buffer1Single(){
+    var lat = $('.lat').text();
+    var lon = $('.lon').text();
+    var url = "http://epamap14.epa.gov/ejmap/demog2010report.aspx?coords=" + lon + "," + lat + "&feattype=point&radius=1";
+    hideEverything();
+    window.open(url, "_blank");
+    if(buffer1SingleLayer._div === null){
+      buffer = new Circle({
+        center: [lon, lat],
+        geodesic: true,
+        radius: 1,
+        radiusUnit: "esriMiles"
+      });
+      var bufferCircle = new Graphic(buffer, bufferSymb);
+      buffer1SingleLayer.add(bufferCircle);
+      $('#legendInfo').html('<p>1 Mile Radius</p>');
+      map.addLayer(buffer1SingleLayer);
+      buffer1SingleLayer.show();
+    }
+    else{
+      buffer1SingleLayer.clear();
+      buffer = new Circle({
+        center: [lon, lat],
+        geodesic: true,
+        radius: 1,
+        radiusUnit: "esriMiles"
+      });
+      var bufferCircle = new Graphic(buffer, bufferSymb);
+      buffer1SingleLayer.add(bufferCircle);
+      $('#legendInfo').html('<p>1 Mile Radius</p>');
+      map.addLayer(buffer1SingleLayer);
+      buffer1SingleLayer.show();
+    }
+  }
+
+  function buffer1(){
+    hideEverything();
+    if(buffer1Layer._div === null){
+      for ( i in latLonArray.points){
+        buffer = new Circle({
+          center: latLonArray.points[i],
+          geodesic: true,
+          radius: 1,
+          radiusUnit: "esriMiles"
+        });
+        var bufferCircle = new Graphic(buffer, bufferSymb);
+        buffer1Layer.add(bufferCircle);
       }
-      else{
-        $('#legendInfo').html('<p>1 Mile Radius</p>');
-        buffer1Layer.show();
+      $('#legendInfo').html('<p>1 Mile Radius</p>');
+      map.addLayer(buffer1Layer);
+      buffer1Layer.show();
+    }
+    else{
+      $('#legendInfo').html('<p>1 Mile Radius</p>');
+      buffer1Layer.show();
+    }
+  }
+
+  function buffer3(){
+    hideEverything();
+    if(buffer3Layer._div === null){
+      for ( i in latLonArray.points){
+        buffer = new Circle({
+          center: latLonArray.points[i],
+          geodesic: true,
+          radius: 3,
+          radiusUnit: "esriMiles"
+        });
+        var bufferCircle = new Graphic(buffer, bufferSymb);
+        buffer3Layer.add(bufferCircle);
       }
-    });
-  
-    $('.buffer3').click(function(){
-      hideEverything("buffer3");
-      if(buffer3Layer._div === null){
-        for ( i in latLonArray.points){
-          buffer = new Circle({
-            center: latLonArray.points[i],
-            geodesic: true,
-            radius: 3,
-            radiusUnit: "esriMiles"
-          });
-          var bufferCircle = new Graphic(buffer, bufferSymb);
-          buffer3Layer.add(bufferCircle);
-        }
-        $('#legendInfo').html('<p>3 Mile Radius</p>');
-        map.addLayer(buffer3Layer);
-        buffer3Layer.show();
-      }
-      else{
-        $('#legendInfo').html('<p>3 Mile Radius</p>');
-        buffer3Layer.show();
-      }
-    });
+      $('#legendInfo').html('<p>3 Mile Radius</p>');
+      map.addLayer(buffer3Layer);
+      buffer3Layer.show();
+    }
+    else{
+      $('#legendInfo').html('<p>3 Mile Radius</p>');
+      buffer3Layer.show();
+    }
+  }
 
     $("#hideLayers").click(function(){
       hideEverything();
+      $('#legendInfo').empty();
     });
 
-    function hideEverything(type){
-      if(type === "buffer1"){
-        buffer3Layer.hide();
-      }
-      else if(type === "buffer3"){
-        buffer1Layer.hide();
-      }
-      else{
-        buffer1Layer.hide();
-        buffer3Layer.hide();
-      }
+    function hideEverything(){
+      buffer1Layer.hide();
+      buffer1SingleLayer.hide();
+      buffer3Layer.hide();
     }
   });
 });
