85279d9995c3515df935bd71468295ab14b06985
areskoga <ali_areskog@abtassoc.com>
2014-08-25 15:18:50 -0400

    changes to calculations for income and race

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 var markers = [
-{ lat:'34.256535', lon:'-92.052418', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602TRFLR5100I" target=_blank>ARCELORMITTAL PINE BLUFF</a>', address: '5100 INDUSTRIAL DR S, PINE BLUFF ARKANSAS 71602 (JEFFERSON), PINE BLUFF, AR 71602<br>', triID: '71602TRFLR5100I', latLonHtml: 'Latitude <span class="lat">34.256535</span>&nbsp; Longitude <span class="lon">-92.052418</span>', label:'ARCELORMITTAL PINE BLUFF', ej1:'0.607,0.448,0.018,0.402,0.581,0.003,0.393,0.167,0.244,0.085', ej3:'0.583,0.263,0.009,0.422,0.560,0.005,0.317,0.186,0.273,0.124' },
-{ lat:'34.257073', lon:'-92.062488', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602FLRDD3300N" target=_blank>BERENFIELD CONTAINERS SW LTD</a>', address: '3300 N HUTCHINSON ST, WHITE HALL ARKANSAS 71602 (JEFFERSON), WHITE HALL, AR 71602', triID: '71602FLRDD3300N', latLonHtml: 'Latitude <span class="lat">34.257073</span>&nbsp; Longitude <span class="lon">-92.062488</span>', label:'BERENFIELD CONTAINERS SW LTD', ej1:'0.376,0.303,0.012,0.632,0.350,0.004,0.297,0.170,0.290,0.108', ej3:'0.514,0.245,0.010,0.491,0.490,0.006,0.299,0.183,0.275,0.136' },
-{ lat:'34.2656', lon:'-92.0261', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71602GYLRD500MC" target=_blank>DELTA NATURAL KRAFT</a>', address: '1701 JEFFERSON PKWY, PINE BLUFF ARKANSAS 71611 (JEFFERSON), PINE BLUFF, AR 71611', triID: '71602GYLRD500MC', latLonHtml: 'Latitude <span class="lat">34.2656</span>&nbsp; Longitude <span class="lon">-92.0261</span>', label:'DELTA NATURAL KRAFT', ej1:'0.973,0.421,0.002,0.027,0.965,0.000,0.296,0.235,0.358,0.037', ej3:'0.706,0.336,0.010,0.298,0.685,0.003,0.373,0.198,0.273,0.081' },
-{ lat:'34.221569', lon:'-91.907406', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71611NTRNTFAIRF" target=_blank>EVERGREEN PACKAGING</a>', address: '5201 FAIRFIELD RD, PINE BLUFF ARKANSAS 71601 (JEFFERSON), PINE BLUFF, AR 71601', triID: '71611NTRNTFAIRF', latLonHtml: 'Latitude <span class="lat">34.221569</span>&nbsp; Longitude <span class="lon">-91.907406</span><br>', label:'EVERGREEN PACKAGING', ej1:'0.500,0.152,0.063,0.531,0.438,0.000,0.231,0.231,0.308,0.077', ej3:'0.529,0.247,0.033,0.482,0.485,0.002,0.268,0.216,0.301,0.098' },
-{ lat:'33.138611', lon:'-91.966667', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=71635GRGPCPAPER" target=_blank>GEORGIA-PACIFIC CROSSETT PLYWOOD/STUDMI LL COMPLEX</a>', address: '101 PLYWOOD MILL RD, CROSSETT ARKANSAS 71635 (ASHLEY), CROSSETT, AR 71635', triID: '71635GRGPCPAPER', latLonHtml: 'Latitude <span class="lat">33.138611</span>&nbsp; Longitude <span class="lon">-91.966667</span>', label:'GEORGIA-PACIFIC CROSSETT PLYWOOD/STUDMI LL COMPLEX', ej1:'0.693,0.284,0.014,0.308,0.672,0.004,0.413,0.094,0.250,0.133', ej3:'0.383,0.176,0.017,0.623,0.359,0.004,0.255,0.129,0.329,0.167' }
-];
+{ lat:'40.03678', lon:'-105.22465', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80301HSRCH5555A" target=_blank>AGILENT TECHNOLOGIES BOULDER CO</a>', address: '5555 AIRPORT BLVD STE 100, BOULDER COLORADO 80301 (BOULDER), BOULDER, CO 80301', triID: '80301HSRCH5555A', latLonHtml: 'Latitude <span class="lat">40.03678</span>&nbsp; Longitude <span class="lon">-105.22465</span>', label:'AGILENT TECHNOLOGIES BOULDER CO', ej1:'0.234,0.103,0.149,0.835,0.015,0.033,0.105,0.112,0.248,0.257', ej3:'0.179,0.156,0.097,0.870,0.014,0.044,0.166,0.105,0.281,0.167' },
+{ lat:'40.238056', lon:'-104.998056', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80542BLDRS5983R" target=_blank>BOULDER SCIENTIFIC CO MEAD FACILITY</a>', address: '598 THIRD ST, MEAD COLORADO 80542 (WELD), MEAD, CO 80542', triID: '80542BLDRS5983R', latLonHtml: 'Latitude <span class="lat">40.238056</span>&nbsp; Longitude <span class="lon">-104.998056</span>',  label:'BOULDER SCIENTIFIC CO MEAD FACILITY', ej1:'0.139,0.031,0.122,0.932,0.000,0.007,0.040,0.070,0.180,0.240', ej3:'0.138,0.030,0.120,0.935,0.001,0.006,0.037,0.068,0.188,0.251' },
+{ lat:'39.78243', lon:'-104.92225', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80216CLRDP4747H" target=_blank>COLORADO PAINT CO</a>', address: '4747 HOLLY ST, DENVER COLORADO 80216 (DENVER), DENVER, CO 80216', triID: '80216CLRDP4747H', latLonHtml: 'Latitude <span class="lat">39.78243</span>&nbsp; Longitude <span class="lon">-104.92225</span>',  label:'COLORADO PAINT CO', ej1:'0.851,0.191,0.272,0.264,0.562,0.006,0.198,0.129,0.412,0.185', ej3:'0.718,0.190,0.396,0.437,0.291,0.011,0.191,0.142,0.317,0.176' },
+{ lat:'40.02025', lon:'-105.227461', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80301SYNTX2075N" target=_blank>CORDEN PHARMA COLORADO INC</a>', address: '2075 N 55TH ST, BOULDER COLORADO 80301 (BOULDER), BOULDER, CO 80301', triID: '80301SYNTX2075N', latLonHtml: 'Latitude <span class="lat">40.02025</span>&nbsp; Longitude <span class="lon">-105.227461</span>',  label:'CORDEN PHARMA COLORADO INC', ej1:'0.160,0.066,0.074,0.874,0.011,0.052,0.073,0.064,0.245,0.213', ej3:'0.177,0.197,0.092,0.869,0.014,0.047,0.188,0.120,0.279,0.169' },
+{ lat:'39.87075', lon:'-104.888006', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80640THNLM851E9" target=_blank>ETHANOL MANAGEMENT CO  LLC</a>', address: '8501 E 96TH AVE, HENDERSON COLORADO 80640 (ADAMS), HENDERSON, CO 80640', triID: '80640THNLM851E9', latLonHtml: 'Latitude <span class="lat">39.87075</span>&nbsp; Longitude <span class="lon">-104.888006</span>',  label:'ETHANOL MANAGEMENT CO  LLC', ej1:'0.396,0.044,0.342,0.767,0.012,0.007,0.117,0.110,0.434,0.223', ej3:'0.364,0.087,0.312,0.775,0.012,0.016,0.090,0.100,0.346,0.275' },
+{ lat:'40.455978', lon:'-104.857553', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80550FRNTR31375" target=_blank>FRONT RANGE ENERGY</a>', address: '31375 GREAT WESTERN DR, WINDSOR COLORADO 80550 (WELD), WINDSOR, CO 80550', triID: '80550FRNTR31375', latLonHtml: 'Latitude <span class="lat">40.455978</span>&nbsp; Longitude <span class="lon">-104.857553</span>',  label:'FRONT RANGE ENERGY', ej1:'0.145,0.061,0.116,0.897,0.004,0.007,0.106,0.098,0.277,0.279', ej3:'0.140,0.061,0.112,0.905,0.004,0.006,0.097,0.096,0.277,0.270' },
+{ lat:'38.276933', lon:'-104.523808', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=81001BFGDR50WIL" target=_blank>GOODRICH CARBON OPERATIONS</a>', address: '50 WILLIAM WHITE BLVD, PUEBLO COLORADO 81001 (PUEBLO), PUEBLO, CO 81001', triID: '81001BFGDR50WIL', latLonHtml: 'Latitude <span class="lat">38.276933</span>&nbsp; Longitude <span class="lon">-104.523808</span>',  label:'GOODRICH CARBON OPERATIONS', ej1:'0.368,0.116,0.337,0.796,0.007,0.004,0.144,0.118,0.492,0.169', ej3:'0.492,0.172,0.454,0.743,0.011,0.008,0.231,0.165,0.328,0.159' },
+{ lat:'40.02726', lon:'-105.23966', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80301NPRBT4884S" target=_blank>HOSPIRA BOULDER INC</a>', address: '4884 STERLING DR, BOULDER COLORADO 80301 (BOULDER), BOULDER, CO 80301', triID: '80301NPRBT4884S', latLonHtml: 'Latitude <span class="lat">40.02726</span>&nbsp; Longitude <span class="lon">-105.23966</span>',  label:'HOSPIRA BOULDER INC', ej1:'0.283,0.163,0.184,0.785,0.023,0.048,0.153,0.139,0.353,0.188', ej3:'0.165,0.185,0.087,0.878,0.013,0.042,0.178,0.117,0.269,0.165' },
+{ lat:'39.79829', lon:'-104.94363', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=81007NTRST646NS" target=_blank>INTERSTATE CHEMICAL CO INC</a>', address: '646 N STATES AVE, PUEBLO WEST COLORADO 81007 (PUEBLO), PUEBLO WEST, CO 81007', triID: '81007NTRST646NS', latLonHtml: 'Latitude <span class="lat">39.79829</span>&nbsp; Longitude <span class="lon">-104.94363</span>',  label:'INTERSTATE CHEMICAL CO INC', ej1:'0.386,0.064,0.327,0.803,0.022,0.022,0.070,0.113,0.338,0.296', ej3:'0.352,0.073,0.296,0.816,0.018,0.020,0.087,0.116,0.350,0.251' },
+{ lat:'39.9695', lon:'-104.81823', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80601LLTCC1095S" target=_blank>INTERTAPE POLYMER GROUP BRIGHTON FACILITY</a>', address: '1095 S FOURTH AVE, BRIGHTON COLORADO 80601 (ADAMS), BRIGHTON, CO 80601', triID: '80601LLTCC1095S', latLonHtml: 'Latitude <span class="lat">39.9695</span>&nbsp; Longitude <span class="lon">-104.81823</span>',  label:'INTERTAPE POLYMER GROUP BRIGHTON FACILITY', ej1:'0.394,0.088,0.358,0.779,0.005,0.016,0.085,0.104,0.319,0.269', ej3:'0.410,0.092,0.376,0.773,0.009,0.011,0.107,0.107,0.321,0.251' },
+{ lat:'40.4525', lon:'-104.869166', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80551KDKCLCOUNT" target=_blank>KODAK COLORADO DIV</a>', address: '9952 EASTMAN PARK DR, WINDSOR COLORADO 80551 (WELD), WINDSOR, CO 80551', triID: '80551KDKCLCOUNT', latLonHtml: 'Latitude <span class="lat">40.4525</span>&nbsp; Longitude <span class="lon">-104.869166</span>',  label:'KODAK COLORADO DIV', ej1:'0.146,0.061,0.116,0.896,0.004,0.007,0.108,0.097,0.278,0.279', ej3:'0.134,0.060,0.108,0.912,0.004,0.006,0.094,0.092,0.274,0.274' },
+{ lat:'40.454722', lon:'-104.866944', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80528MSKTW23HWA" target=_blank>MUSKET WINDSOR TRANSLOADING FACILITY</a>', address: '2030 HOWARD SMITH AVE E, WINDSOR COLORADO 80528 (WELD), WINDSOR, CO 80528', triID: '80528MSKTW23HWA', latLonHtml: 'Latitude <span class="lat">40.454722</span>&nbsp; Longitude <span class="lon">-104.866944</span>', label:'MUSKET WINDSOR TRANSLOADING FACILITY', ej1:'', ej3:'' },
+{ lat:'39.89144', lon:'-104.86917', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80601SSHCN10300" target=_blank>SASHCO INC</a>', address: '10300 E 107TH PL, BRIGHTON COLORADO 80601 (ADAMS), BRIGHTON, CO 80601', triID: '80601SSHCN10300', latLonHtml: 'Latitude <span class="lat">39.89144</span>&nbsp; Longitude <span class="lon">-104.86917</span>', label:'SASHCO INC', ej1:'0.273,0.045,0.233,0.840,0.007,0.017,0.069,0.050,0.277,0.198', ej3:'0.271,0.052,0.219,0.838,0.010,0.019,0.071,0.069,0.305,0.282' },
+{ lat:'39.78339', lon:'-104.93863', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80216SHFRC4101E" target=_blank>SHAFER COMMERCIAL SEATING</a>', address: '4101 E 48TH AVE, DENVER COLORADO 80216 (DENVER), DENVER, CO 80216', triID: '80216SHFRC4101E', latLonHtml: 'Latitude <span class="lat">39.78339</span>&nbsp; Longitude <span class="lon">-104.93863</span>', label:'SHAFER COMMERCIAL SEATING', ej1:'0.900,0.277,0.709,0.329,0.176,0.005,0.238,0.164,0.335,0.181', ej3:'0.732,0.203,0.405,0.426,0.297,0.010,0.209,0.149,0.312,0.170' },
+{ lat:'39.87075', lon:'-104.88672', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80640SNCLR8581E" target=_blank>SINCLAIR DENVER PRODUCTS TERMINAL</a>', address: '8581 E 96TH AVE, HENDERSON COLORADO 80640 (ADAMS), HENDERSON, CO 80640', triID: '80640SNCLR8581E', latLonHtml: 'Latitude <span class="lat">39.87075</span>&nbsp; Longitude <span class="lon">-104.88672</span>', label:'SINCLAIR DENVER PRODUCTS TERMINAL', ej1:'0.398,0.042,0.344,0.766,0.011,0.007,0.116,0.109,0.434,0.222', ej3:'0.366,0.088,0.314,0.774,0.012,0.016,0.092,0.101,0.348,0.273' },
+{ lat:'39.77642', lon:'-104.9223', nameLink:'<a href="http://oaspub.epa.gov/enviro/tris_control.tris_print?tris_id=80216VNWTR4300H" target=_blank>UNIVAR USA INC DENVER</a>', address: '4300 HOLLY ST, DENVER COLORADO 80216 (DENVER), DENVER, CO 80216', triID: '80216VNWTR4300H', latLonHtml: 'Latitude <span class="lat">39.77642</span>&nbsp; Longitude <span class="lon">-104.9223</span>', label:'UNIVAR USA INC DENVER', ej1:'0.938,0.199,0.224,0.163,0.697,0.006,0.256,0.123,0.366,0.170', ej3:'0.685,0.189,0.361,0.456,0.289,0.013,0.192,0.144,0.311,0.173' }
+  ];
 
 var fullscreen;
 
@@ -427,7 +438,9 @@ $(document).ready(function(){
   var element = $("#grid").kendoGrid({
     dataSource: data,
     sortable: true,
-    pageable: true,
+    pageable: {
+      pageSize: 10
+    },
     detailTemplate: kendo.template($("#template").html()),
     detailInit: detailInit,
     columns: [
@@ -484,12 +497,13 @@ function detailInit(e) {
     for(i in markers){
       var string1 = markers[i].ej1;
       var allData1 = string1.split(",");
-      var income5_1 = 1-(parseInt(allData1[6])+parseInt(allData1[7])+parseInt(allData1[8])+parseInt(allData1[9]));
+      var income5_1 = 1-(parseFloat(allData1[6])+parseFloat(allData1[7])+parseFloat(allData1[8])+parseFloat(allData1[9]));
+      var raceOther_1 = 1-(parseFloat(allData1[3])+parseFloat(allData1[4])+parseFloat(allData1[5]));
       var minorityData1 = [{ category: "Minority", value: allData1[0] }, {category: "Other", value: 1-allData1[0], valueColor: "#000"}];
       var povertyData1 = [{ category: "Poverty", value: allData1[1] }, {category: "Other", value: 1-allData1[1], valueColor: "#000"}];
       var hispanicData1 = [{ category: "Hispanic", value: allData1[2] }, {category: "Other", value: 1-allData1[2], valueColor: "#000"}];
-      var incomeData1 = [{ category: "Income1", value: allData1[6] },{ category: "Income2", value: allData1[7] }, { category: "Income3", value: allData1[8] }, { category: "Income4", value: allData1[9] }, { category: "Income5", value: income5_1 } ];
-      var raceData1 = [{ category: "Hispanic", value: allData1[2] },{ category: "White", value: allData1[3] }, { category: "Black", value: allData1[4] }, { category: "Asian", value: allData1[5] } ];
+      var incomeData1 = [{ category: "< $15k", value: allData1[6] },{ category: "$15k-$25k", value: allData1[7] }, { category: "$25k-$50k", value: allData1[8] }, { category: "$50k-$75k", value: allData1[9] }, { category: "> $75k", value: income5_1 } ];
+      var raceData1 = [{ category: "White", value: allData1[3] }, { category: "Black", value: allData1[4] }, { category: "Asian", value: allData1[5] }, { category: "Other", value: raceOther_1 } ];
 
       var panelNum = parseInt(i) + 1;
       createPie2("panel" + panelNum, "minority1", minorityData1, "Percent Minority");
@@ -500,12 +514,13 @@ function detailInit(e) {
 
       var string3 = markers[i].ej3;
       var allData3 = string3.split(",");
-      var income5_3 = 1-(parseInt(allData3[6])+parseInt(allData3[7])+parseInt(allData3[8])+parseInt(allData3[9]));
+      var income5_3 = 1-(parseFloat(allData3[6])+parseFloat(allData3[7])+parseFloat(allData3[8])+parseFloat(allData3[9]));
+      var raceOther_3 = 1-(parseFloat(allData3[3])+parseFloat(allData3[4])+parseFloat(allData3[5]));
       var minorityData3 = [{ category: "Minority", value: allData3[0] }, {category: "Other", value: 1-allData3[0], valueColor: "#000"}];
       var povertyData3 = [{ category: "Poverty", value: allData3[1] }, {category: "Other", value: 1-allData3[1], valueColor: "#000"}];
       var hispanicData3 = [{ category: "Hispanic", value: allData3[2] }, {category: "Other", value: 1-allData3[2], valueColor: "#000"}];
-      var incomeData3 = [{ category: "Income1", value: allData3[6] },{ category: "Income2", value: allData3[7] }, { category: "Income3", value: allData3[8] }, { category: "Income4", value: allData3[9] }, { category: "Income5", value: income5_3 } ];
-      var raceData3 = [{ category: "Hispanic", value: allData3[2] },{ category: "White", value: allData3[3] }, { category: "Black", value: allData3[4] }, { category: "Asian", value: allData3[5] } ];
+      var incomeData3 = [{ category: "< $15k", value: allData3[6] },{ category: "$15k-$25k", value: allData3[7] }, { category: "$25k-$50k", value: allData3[8] }, { category: "$50k-$75k", value: allData3[9] }, { category: "> $75k", value: income5_3 } ];
+      var raceData3 = [{ category: "White", value: allData3[3] }, { category: "Black", value: allData3[4] }, { category: "Asian", value: allData3[5] }, { category: "Other", value: raceOther_3 } ];
 
       createPie2("panel_2_" + panelNum, "minority3", minorityData3, "Percent Minority");
       createPie2("panel_2_" + panelNum, "poverty3", povertyData3, "Poverty");
@@ -573,7 +588,7 @@ function income1(){
   var item = $('#markerNum').data('number');
   var string1 = markers[item].ej1;
   var allData1 = string1.split(",");
-  var income5_1 = 1-(parseInt(allData1[6])+parseInt(allData1[7])+parseInt(allData1[8])+parseInt(allData1[9]));
+  var income5_1 = 1-(parseFloat(allData1[6])+parseFloat(allData1[7])+parseFloat(allData1[8])+parseFloat(allData1[9]));
   var incomeData1 = [{ category: "< $15k", value: allData1[6] },{ category: "$15k-$25k", value: allData1[7] }, { category: "$25k-$50k", value: allData1[8] }, { category: "$50-75k", value: allData1[9] }, { category: "> $75k", value: income5_1 } ];
   $('#demographicChart').show();
   createPie(incomeData1, "Income Data 1 Mile");
@@ -588,7 +603,7 @@ function income3(){
   var item = $('#markerNum').data('number');
   var string3 = markers[item].ej3;
   var allData3 = string3.split(",");
-  var income5_3 = 1-(parseInt(allData3[6])+parseInt(allData3[7])+parseInt(allData3[8])+parseInt(allData3[9]));
+  var income5_3 = 1-(parseFloat(allData3[6])+parseFloat(allData3[7])+parseFloat(allData3[8])+parseFloat(allData3[9]));
   var incomeData3 = [{ category: "< $15k", value: allData3[6] },{ category: "$15k-$25k", value: allData3[7] }, { category: "$25k-$50k", value: allData3[8] }, { category: "$50k-$75k", value: allData3[9] }, { category: "> $75k", value: income5_3 } ];
   $('#demographicChart').show();
   createPie(incomeData3, "Income Data 3 mile");
@@ -599,7 +614,8 @@ function race1(){
   var item = $('#markerNum').data('number');
   var string1 = markers[item].ej1;
   var allData1 = string1.split(",");
-  var raceData1 = [{ category: "Hispanic", value: allData1[2] },{ category: "White", value: allData1[3] }, { category: "Black", value: allData1[4] }, { category: "Asian", value: allData1[5] } ];
+  var raceOther_1 = 1-(parseFloat(allData1[3])+parseFloat(allData1[4])+parseFloat(allData1[5]));
+  var raceData1 = [{ category: "White", value: allData1[3] }, { category: "Black", value: allData1[4] }, { category: "Asian", value: allData1[5] }, { category: "Other", value: raceOther_1} ];
   $('#demographicChart').show();
   createPie(raceData1, "Race 1 Mile");
   location.hash = "#demographicChart";
@@ -609,7 +625,8 @@ function race3(){
   var item = $('#markerNum').data('number');
   var string3 = markers[item].ej3;
   var allData3 = string3.split(",");
-  var raceData3 = [{ category: "Hispanic", value: allData3[2] },{ category: "White", value: allData3[3] }, { category: "Black", value: allData3[4] }, { category: "Asian", value: allData3[5] } ];
+  var raceOther_3 = 1-(parseFloat(allData3[3])+parseFloat(allData3[4])+parseFloat(allData3[5]));
+  var raceData3 = [{ category: "White", value: allData3[3] }, { category: "Black", value: allData3[4] }, { category: "Asian", value: allData3[5] }, { category: "Other", value: raceOther_3} ];
   $('#demographicChart').show();
   createPie(raceData3, "Race 3 Mile");
   location.hash = "#demographicChart";
@@ -684,7 +701,7 @@ function createPie2(id, className, data, title){
     },
     tooltip: {
       visible: true,
-      template: "#= category # - #= kendo.format('{0:P}', percentage) #"
+      template: "#= category #: #= kendo.format('{0:P}', percentage) #"
     },
     seriesColors: ["#d7191c", "#fdae61", "#ffffbf", "#abd9e9", "#2c7bb6"]
   });
